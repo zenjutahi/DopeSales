5db1045d81b3ceea96f0b97cc8c459b752212031
Zenjutahi <zenjutahi@gmail.com>
2019-09-02 17:37:07 +0300

    [feature] - global styling using injectGlobal - styled componets

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -8,7 +8,7 @@ const Logo = styled.h1`
     margin-left: 2rem;
     position: relative;
     z-index: 2;
-    transform: skew(-7deg);
+    transform: skew(-8deg);
     a {
         padding: 0.5rem 1rem;
         background: ${props => props.theme.blue};
