7b1337de3dded25f6264c8136d3a3e9ea2a30b47
Zenjutahi <zenjutahi@gmail.com>
2019-09-04 17:04:38 +0300

    [chore] - connect Js with remote prisma (graphQl Yoga)

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,8 +1,8 @@
-//This file connects with the remote prisma and enable sus to query it in JS
+//This file connects with the remote prisma and enables us to query it in JS
 const { Prisma } = require('prisma-binding');
 
 const db = new Prisma({
-    typeDefs: 'src/generated/prisma.grapgql',
+    typeDefs: 'src/generated/prisma.graphql',
     endpoint: process.env.PRISMA_ENDPOINT,
     secret: process.env.PRISMA_SECRET,
     debug: false,
