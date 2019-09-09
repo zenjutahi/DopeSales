bced996743b5df969d8ccdb45e6c3bbda7b2e7da
Zenjutahi <zenjutahi@gmail.com>
2019-09-09 18:03:07 +0300

    [feature] - creating items with mutations

---
+++ frontend/components/CreateItem.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+import { Mutation } from 'react-apollo';
+import gql from 'graphql-tag';
+import Form from './styles/Form';
+import formatMoney from '../lib/formatMoney';
+
+
+class CreateItem extends Component {
+    state = {
+        title: '',
+        description: '',
+        image: '',
+        largeImage: '',
+        price: 0,
+    };
+    render() {
+        return (
+            <Form>
+                <fieldset>
+                    <label htmlFor="title">
+                        Title
+                        <input type="text" id="title" name="title"
+                        placeholder="Title" required
+                        value= {this.state.title} />
+                    </label>
+                </fieldset>
+            </Form>
+        );
+    }
+}
+
+export default CreateItem;
