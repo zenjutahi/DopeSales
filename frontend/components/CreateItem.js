8b46280a33f0e08db697f7eef33ada8ba798db8e
Zenjutahi <zenjutahi@gmail.com>
2019-09-10 12:18:25 +0300

    [feature] - creating items with mutations

--- frontend/components/CreateItem.js
+++ frontend/components/CreateItem.js
@@ -29,23 +29,41 @@ const CREATE_ITEM_MUTATION = gql`
     }
 `;
 
-
 class CreateItem extends Component {
     state = {
         title: 'Cool Socks',
         description: 'In general, the details of Aristotles life are not well-established',
-        image: 'dog.jpeg',
-        largeImage: 'large-dog.jpeg',
+        image: '',
+        largeImage: '',
         price: 96730,
     };
-    handleChage = e => {
+    handleChange = e => {
         const { name, type, value } = e.target;
         const val = type === 'number' ? parseFloat(value) :
         value; 
         this.setState({ [name]: val });
-    }
+    };
 
+    uploadFile = async e => {
+        console.log('uploading file...');
+        const files = e.target.files;
+        const data = new FormData();
+        data.append('file', files[0]);
+        data.append('upload_preset', 'dopesales');
 
+        const res = await fetch
+            ('https://api.cloudinary.com/v1_1/dse2w5id1/image/upload/',
+            {
+                method: 'POST',
+                body: data
+            });
+        const file = await res.json();
+        console.log(file);
+        this.setState({
+            image: file.secure_url,
+            largeImage: file.eager[0].secure_url
+        });
+    };
     render() {
         return (
             <Mutation mutation={CREATE_ITEM_MUTATION} variables=
@@ -65,6 +83,18 @@ class CreateItem extends Component {
                 }}>
                     <Error error={error} />
                     <fieldset disabled={loading} aria-busy={loading}>
+                        <label htmlFor="file">
+                            Image
+                            <input type="file" 
+                            id="file" 
+                            name="file"
+                            placeholder="Upload an image" 
+                            required
+                            onChange={this.uploadFile}
+                            />
+                            {this.state.image && <img src=
+                            {this.state.image} alt="Uplaod Preview" />}
+                        </label>
                         <label htmlFor="title">
                             Title
                             <input type="text" 
@@ -73,7 +103,7 @@ class CreateItem extends Component {
                             placeholder="Title" 
                             required
                             value= {this.state.title}
-                            onChange={this.handleChage}
+                            onChange={this.handleChange}
                             />
                         </label>
                         <label htmlFor="price">
@@ -84,7 +114,7 @@ class CreateItem extends Component {
                             placeholder="Price" 
                             required
                             value= {this.state.price}
-                            onChange={this.handleChage}
+                            onChange={this.handleChange}
                             />
                         </label>
                         <label htmlFor="description">
@@ -95,7 +125,7 @@ class CreateItem extends Component {
                             placeholder="Enter a Description" 
                             required
                             value= {this.state.description}
-                            onChange={this.handleChage}
+                            onChange={this.handleChange}
                             />
                         </label>
                         <button type="submit">Submit</button>
