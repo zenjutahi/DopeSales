886a81ed32ab3c40d40195142cf6a594ae384f11
Zenjutahi <zenjutahi@gmail.com>
2019-09-16 10:50:35 +0300

    [chore] - restructure Items view

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -1,29 +1,27 @@
-import React, { Component } from 'react';
-import styled, { ThemeProvider ,injectGlobal }from 'styled-components';
-import Header from '../components/Header';
-import Meta from '../components/Meta';
-
-
+import React, { Component } from "react";
+import styled, { ThemeProvider, injectGlobal } from "styled-components";
+import Header from "../components/Header";
+import Meta from "../components/Meta";
 
 const theme = {
-    blue: '#87CEFA',
-    black: '#393939',
-    grey: '#3A3A3A',
-    lightgrey: '#E1E1E1',
-    offWhite: '#EDEDED',
-    maxWidth: '1000px',
-    bs: '0 12px 24px 0 rgba(0, 0, 0, 0.09)',
+  blue: "#87CEFA",
+  black: "#393939",
+  grey: "#3A3A3A",
+  lightgrey: "#E1E1E1",
+  offWhite: "#EDEDED",
+  maxWidth: "1000px",
+  bs: "0 12px 24px 0 rgba(0, 0, 0, 0.09)"
 };
 
 const StyledPage = styled.div`
-    background: white;
-    color: ${props => props.theme.black};
+  background: white;
+  color: ${props => props.theme.black};
 `;
 
 const Inner = styled.div`
-    max-width: ${props => props.theme.maxWidth};
-    margin: 0 auto;
-    padding: 2rem;
+  max-width: ${props => props.theme.maxWidth};
+  margin: 0 auto;
+  padding: 2rem;
 `;
 
 injectGlobal`
@@ -54,19 +52,18 @@ injectGlobal`
     }
 `;
 
-
 class Page extends Component {
-    render() {
-        return (
-            <ThemeProvider theme={theme}>
-                <StyledPage>
-                    <Meta />
-                    <Header />
-                    <Inner>{this.props.children}</Inner>
-                </StyledPage>
-            </ThemeProvider>
-        );
-    }
+  render() {
+    return (
+      <ThemeProvider theme={theme}>
+        <StyledPage>
+          <Meta />
+          <Header />
+          <Inner>{this.props.children}</Inner>
+        </StyledPage>
+      </ThemeProvider>
+    );
+  }
 }
 
 export default Page;
