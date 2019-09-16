36d9165924bbd0b3cf6af00c38b399b52f57a00e
Zenjutahi <zenjutahi@gmail.com>
2019-09-16 10:52:24 +0300

    [chore] - restructure Items view

---
+++ frontend/components/Pagination.js
@@ -0,0 +1,10 @@
+import React from "react";
+import PaginationStyles from "./styles/PaginationStyles";
+
+const Pagination = props => (
+  <PaginationStyles>
+    <p>Hello I'm the pagination!</p>
+  </PaginationStyles>
+);
+
+export default Pagination;
