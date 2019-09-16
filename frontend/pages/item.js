236b4420d43a970217d3c7db4f1d13c8d9aa4f79
Zenjutahi <zenjutahi@gmail.com>
2019-09-16 10:51:44 +0300

    [chore] - restructure Items view

--- frontend/pages/item.js
+++ frontend/pages/item.js
@@ -1,10 +1,9 @@
-import SingleItem from '../components/SingleItem';
+import SingleItem from "../components/SingleItem";
 
 const Item = props => (
-    <div>
-        <SingleItem id={props.query.id} />
-    </div>
+  <div>
+    <SingleItem id={props.query.id} />
+  </div>
 );
 
 export default Item;
-
