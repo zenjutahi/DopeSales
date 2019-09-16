bc3dbe927a9b1bf2d483b4e8daa0774e1dee1d5c
Zenjutahi <zenjutahi@gmail.com>
2019-09-16 10:49:50 +0300

    [chore] - restructure Items view

--- frontend/components/Item.js
+++ frontend/components/Item.js
@@ -1,45 +1,47 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
-import Link from 'next/link';
-import Title from './styles/Title';
-import Itemstyles from './styles/ItemStyles';
-import PriceTag from './styles/PriceTag';
-import formatMoney from '../lib/formatMoney';
-import DeleteItem from './DeleteItem';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import Link from "next/link";
+import Title from "./styles/Title";
+import Itemstyles from "./styles/ItemStyles";
+import PriceTag from "./styles/PriceTag";
+import formatMoney from "../lib/formatMoney";
+import DeleteItem from "./DeleteItem";
 
 export default class Item extends Component {
-    static propTypes = {
-        item: PropTypes.object.isRequired,
-    };
+  static propTypes = {
+    item: PropTypes.object.isRequired
+  };
 
-    render() {
-        const { item } = this.props;
-        return (
-            <Itemstyles>
-                {item.image && <img src={item.image} alt=
-                {item.title}/>}
-                <Title>
-                <Link href={{
-                    pathname: '/item',
-                    query: {id: item.id },
-                }}>
-                    <a>{item.title}</a>
-                </Link>
-                </Title>
-                <PriceTag>{formatMoney(item.price)}</PriceTag>
-                <p>{item.description}</p>  
-                <div className="buttonList">
-                    <Link href={{
-                        pathname: 'update',
-                        query: {id: item.id },
-                    }}>
-                        <a>Edit ✏️</a>
-                    </Link>
-                    <button>Add To Cart</button>
-                    <DeleteItem id={item.id}>Delete 🗑️</DeleteItem>
-                </div>
-            </Itemstyles>
-        );
-    }
+  render() {
+    const { item } = this.props;
+    return (
+      <Itemstyles>
+        {item.image && <img src={item.image} alt={item.title} />}
+        <Title>
+          <Link
+            href={{
+              pathname: "/item",
+              query: { id: item.id }
+            }}
+          >
+            <a>{item.title}</a>
+          </Link>
+        </Title>
+        <PriceTag>{formatMoney(item.price)}</PriceTag>
+        <p>{item.description}</p>
+        <div className="buttonList">
+          <Link
+            href={{
+              pathname: "update",
+              query: { id: item.id }
+            }}
+          >
+            <a>Edit ✏️</a>
+          </Link>
+          <button>Add To Cart</button>
+          <DeleteItem id={item.id}>Delete 🗑️</DeleteItem>
+        </div>
+      </Itemstyles>
+    );
+  }
 }
-
