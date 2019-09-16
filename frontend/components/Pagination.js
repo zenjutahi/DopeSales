9e08bfaed8c5586cf02b6c94287db868a35c7c66
Zenjutahi <zenjutahi@gmail.com>
2019-09-16 13:53:36 +0300

    [feature] - add pagination logic

--- frontend/components/Pagination.js
+++ frontend/components/Pagination.js
@@ -1,10 +1,65 @@
 import React from "react";
+import gql from "graphql-tag";
+import Head from "next/head";
+import Link from "next/link";
+import { Query } from "react-apollo";
 import PaginationStyles from "./styles/PaginationStyles";
+import { perPage } from "../config";
+
+const PAGINATION_QUERY = gql`
+  query PAGINATION_QUERY {
+    itemsConnection {
+      aggregate {
+        count
+      }
+    }
+  }
+`;
 
 const Pagination = props => (
-  <PaginationStyles>
-    <p>Hello I'm the pagination!</p>
-  </PaginationStyles>
+  <Query query={PAGINATION_QUERY}>
+    {({ data, loading, error }) => {
+      if (loading) return <p>Loading...</p>;
+      const count = data.itemsConnection.aggregate.count;
+      const pages = Math.ceil(count / perPage);
+      const page = props.page;
+      return (
+        <PaginationStyles>
+          <Head>
+            <title>
+              Dope Sales! - Page {page} of {pages}
+            </title>
+          </Head>
+          <Link
+            prefetch
+            href={{
+              pathname: "items",
+              query: { page: page - 1 }
+            }}
+          >
+            <a className="prev" aria-disabled={page <= 1}>
+              👈 Prev
+            </a>
+          </Link>
+          <p>
+            Page {page} of {pages}
+          </p>
+          <p>{count} Items Total</p>
+          <Link
+            prefetch
+            href={{
+              pathname: "items",
+              query: { page: page + 1 }
+            }}
+          >
+            <a className="prev" aria-disabled={page >= pages}>
+              Next 👉
+            </a>
+          </Link>
+        </PaginationStyles>
+      );
+    }}
+  </Query>
 );
 
 export default Pagination;
