d848596a5fd52cb75d5eff9c143679b948495acd
Zenjutahi <zenjutahi@gmail.com>
2019-09-18 14:21:39 +0300

    [feature] - signup and permissions setup

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,23 +1,24 @@
-require('dotenv').config({ path: 'variables.env' });
-const createServer = require('./createServer');
-const db = require('./db');
-
+const cookieParser = require("cookie-parser");
+require("dotenv").config({ path: "variables.env" });
+const createServer = require("./createServer");
+const db = require("./db");
 
 const server = createServer();
 
-// TODO use express middlwware to handle cookies (JWT)
+// use express middleware to handle cookies (JWT) ie cookieParser is an express middleware
+server.express.use(cookieParser());
+
 // TODO use express middleware to populate current user
 
-
 server.start(
-    {
-        cors: {
-            credentials: true,
-            origin: process.env.FRONTEND_URL,
-        },
-    },
-    deets => {
-        console.log(`Server is now running on port
-        http:/localhost:${deets.port}`);
+  {
+    cors: {
+      credentials: true,
+      origin: process.env.FRONTEND_URL
     }
+  },
+  deets => {
+    console.log(`Server is now running on port
+        http:/localhost:${deets.port}`);
+  }
 );
