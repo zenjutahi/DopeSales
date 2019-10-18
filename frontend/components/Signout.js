c6c92e5faffbee38570aef287ab600f51f7fa166
Zenjutahi <zenjutahi@gmail.com>
2019-10-18 11:07:44 +0300

    [feature] - add signout mutaion and button

--- frontend/components/Signout.js
+++ frontend/components/Signout.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
+import Router from "next/router";
 import { Mutation } from "react-apollo";
+import FlashMessage from "react-flash-message";
 import gql from "graphql-tag";
 import { CURRENT_USER_QUERY } from "./User";
 
@@ -11,13 +13,38 @@ const SIGN_OUT_MUTATION = gql`
   }
 `;
 
-const Signout = props => (
-  <Mutation
-    mutation={SIGN_OUT_MUTATION}
-    refetchQueries={[{ query: CURRENT_USER_QUERY }]}
-  >
-    {signout => <a onClick={signout}>Sign Out</a>}
-  </Mutation>
-);
+class Signout extends Component {
+  render() {
+    return (
+      <Mutation
+        mutation={SIGN_OUT_MUTATION}
+        refetchQueries={[{ query: CURRENT_USER_QUERY }]}
+      >
+        {signout => {
+          return (
+            <a
+              onClick={async e => {
+                e.preventDefault();
+                const res = await signout();
+                const message = res.data.signout.message;
+                console.log(message);
+                if (message) {
+                  Router.push(`/signup`);
+                  // return (
+                  //   <FlashMessage duration={4000} persistOnHover={true}>
+                  //     <p>{message}</p>
+                  //   </FlashMessage>
+                  // );
+                }
+              }}
+            >
+              Sign Out
+            </a>
+          );
+        }}
+      </Mutation>
+    );
+  }
+}
 
 export default Signout;
