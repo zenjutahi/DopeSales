862dc86b508d4be02d386a88fa6173bc6aa3dfb5
Zenjutahi <zenjutahi@gmail.com>
2019-10-18 15:33:16 +0300

    [feature] - reset password functionality i.e form/frontend

---
+++ frontend/components/RequestReset.js
@@ -0,0 +1,81 @@
+import React, { Component } from "react";
+import { Mutation } from "react-apollo";
+import styled from "styled-components";
+import FlashMessage from "react-flash-message";
+import gql from "graphql-tag";
+import Form from "./styles/Form";
+import Error from "./ErrorMessage";
+
+const Smessage = styled.div`
+  border: 1px solid #d8d8d8;
+  padding: 10px;
+  border-radius: 5px;
+  font-family: Arial;
+  font-size: 11px;
+  text-transform: uppercase;
+  background-color: rgb(236, 255, 216);
+  color: green;
+  text-align: center;
+  margin-top: -10px;
+`;
+
+const REQUEST_RESET_MUTATION = gql`
+  mutation REQUEST_RESET_MUTATION($email: String!) {
+    requestReset(email: $email) {
+      message
+    }
+  }
+`;
+
+class RequestReset extends Component {
+  state = {
+    email: ""
+  };
+  saveToState = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+  render() {
+    return (
+      <Mutation mutation={REQUEST_RESET_MUTATION} variables={this.state}>
+        {(requestReset, { error, loading, called }) => {
+          return (
+            <Form
+              method="post"
+              onSubmit={async e => {
+                e.preventDefault();
+                const res = await requestReset();
+                console.log(res);
+                this.setState({ email: "" });
+              }}
+            >
+              <fieldset disabled={loading} aria-busy={loading}>
+                <h2>Request a password reset</h2>
+                <Error error={error} />
+                {!error && !loading && called && (
+                  <FlashMessage duration={4000}>
+                    <Smessage>
+                      Success! Check your email for the reset link!
+                    </Smessage>
+                  </FlashMessage>
+                )}
+                <label htmlFor="email">
+                  Email
+                  <input
+                    type="email"
+                    name="email"
+                    placeholder="email"
+                    value={this.state.email}
+                    onChange={this.saveToState}
+                  />
+                </label>
+                <button type="submit">Request Reset</button>
+              </fieldset>
+            </Form>
+          );
+        }}
+      </Mutation>
+    );
+  }
+}
+
+export default RequestReset;
