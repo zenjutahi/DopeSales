5374459ccd4e696d9aa03b15df39b4adc4265096
Zenjutahi <zenjutahi@gmail.com>
2019-10-21 17:36:46 +0300

    [feature] - data relation addition

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,12 +1,11 @@
 //This file connects with the remote prisma and enables us to query it in JS
-const { Prisma } = require('prisma-binding');
+const { Prisma } = require("prisma-binding");
 
 const db = new Prisma({
-    typeDefs: 'src/generated/prisma.graphql',
-    endpoint: process.env.PRISMA_ENDPOINT,
-    secret: process.env.PRISMA_SECRET,
-    debug: false,
+  typeDefs: "src/generated/prisma.graphql",
+  endpoint: process.env.PRISMA_ENDPOINT,
+  secret: process.env.PRISMA_SECRET,
+  debug: false
 });
-
 
 module.exports = db;
