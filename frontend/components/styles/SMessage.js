a306405afb5e91780f2e33488779b129db3e6f2a
Zenjutahi <zenjutahi@gmail.com>
2019-10-22 10:23:03 +0300

    [feature] - add signin component wrapper

---
+++ frontend/components/styles/SMessage.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const SMessage = styled.div`
+  border: 1px solid #d8d8d8;
+  padding: 10px;
+  border-radius: 5px;
+  font-family: Arial;
+  font-size: 11px;
+  text-transform: uppercase;
+  background-color: rgb(236, 255, 216);
+  color: green;
+  text-align: center;
+  margin-top: -10px;
+`;
+
+export default SMessage;
