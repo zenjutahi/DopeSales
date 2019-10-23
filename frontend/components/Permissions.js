7840441ba575a956c72f5b68537b105ffbd5276d
Zenjutahi <zenjutahi@gmail.com>
2019-10-23 22:55:18 +0300

    [feature] - add all users query

--- frontend/components/Permissions.js
+++ frontend/components/Permissions.js
@@ -80,7 +80,10 @@ class UserPermissions extends React.Component {
         {possiblePermissions.map(permission => (
           <td key={permission}>
             <label htmlFor={`${user.id}-permission-${permission}`}>
-              <input type="checkbox" />
+              <input
+                type="checkbox"
+                checked={this.state.permissions.includes(permission)}
+              />
             </label>
           </td>
         ))}
