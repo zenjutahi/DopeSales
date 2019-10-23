ae11f15a119558c873b29262d0f57bb15b161c1e
Zenjutahi <zenjutahi@gmail.com>
2019-10-23 21:51:19 +0300

    [feature] - add all users query

--- frontend/components/styles/SickButton.js
+++ frontend/components/styles/SickButton.js
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 const SickButton = styled.button`
   background: ${props => props.theme.blue};
+  cursor: pointer;
   color: white;
   font-weight: 500;
   border: 0;
