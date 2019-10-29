58b986c71ea660ce06a128ebf3ef01f3a8405ce1
Zenjutahi <zenjutahi@gmail.com>
2019-10-29 12:24:37 +0300

    [feature] - permisions update to DB

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -3,6 +3,7 @@ const jwt = require("jsonwebtoken");
 const { randomBytes } = require("crypto");
 const { promisify } = require("util");
 const { transport, makeANiceEmail } = require("../mail");
+const { hasPermission } = require("../utils");
 
 const Mutations = {
   async createItem(parent, args, ctx, info) {
@@ -177,9 +178,34 @@ const Mutations = {
   },
   async updatePermissions(parent, args, ctx, info) {
     // 1. Check if they are logged in
+    if (!ctx.request.userId) {
+      throw new Error("You must be logged in to complete that action!! 😉😉");
+    }
     // 2. Query the current user
-    // 3. Check if they have permissions o do this
-    // 4. Update he permissions
+    const currentUser = await ctx.db.query.user(
+      {
+        where: {
+          id: ctx.request.userId
+        }
+      },
+      info
+    );
+    // 3. Check if they have permissions to do this
+    hasPermission(currentUser, ["ADMIN", "PERMISSIONUPDATE", "BOSS"]);
+    // 4. Update the permissions
+    return ctx.db.mutation.updateUser(
+      {
+        data: {
+          permissions: {
+            set: args.permissions
+          }
+        },
+        where: {
+          id: args.userId
+        }
+      },
+      info
+    );
   }
 };
 
