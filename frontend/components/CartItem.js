38930bdf20c7fbe1701807e69ed97c33ac305025
Zenjutahi <zenjutahi@gmail.com>
2019-10-30 18:13:50 +0300

    [feature] - display cart items and totals

--- frontend/components/CartItem.js
+++ frontend/components/CartItem.js
@@ -3,9 +3,36 @@ import styled from "styled-components";
 import PropTypes from "prop-types";
 import formatMoney from "../lib/formatMoney";
 
-const CartItemStyles = styled.li``;
+const CartItemStyles = styled.li`
+  padding: 1rem 0;
+  border-bottom: 1px solid ${props => props.theme.lightgrey};
+  display: grid;
+  align-items: center;
+  grid-template-columns: auto 1fr auto;
+  img {
+    margin-right: 10px;
+  }
+  h3,
+  p {
+    margin: 0;
+  }
+`;
 
-const CartItem = props => <CartItemStyles>{props.cartItem.id}</CartItemStyles>;
+const CartItem = ({ cartItem }) => (
+  <CartItemStyles>
+    <img width="120" src={cartItem.item.image} alt={cartItem.item.title} />
+    <div className="cart-item-details">
+      <h3>{cartItem.item.title}</h3>
+      <p>
+        {formatMoney(cartItem.item.price * cartItem.quantity)}
+        {" - "}
+        <em>
+          {cartItem.quantity} &times; {formatMoney(cartItem.item.price)} each
+        </em>
+      </p>
+    </div>
+  </CartItemStyles>
+);
 
 CartItem.propTypes = {
   cartItem: PropTypes.object.isRequired
