dc4402bfabc3541eef2ca2c3664183b5bfa6f05e
Zenjutahi <zenjutahi@gmail.com>
2019-10-30 11:22:49 +0300

    [feature] - add cart component styles

--- frontend/components/styles/CloseButton.js
+++ frontend/components/styles/CloseButton.js
@@ -1,13 +1,14 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 const CloseButton = styled.button`
   background: black;
   color: white;
-  font-size: 3rem;
-  border: 0;
+  font-size: 3.3rem;
+  border: -1px;
   position: absolute;
   z-index: 2;
   right: 0;
+  top: 0;
 `;
 
 export default CloseButton;
