d2f7ea947e0f1c9dae5292849942b1dc54dcd865
Zenjutahi <zenjutahi@gmail.com>
2019-10-30 12:51:19 +0300

    [feature] - working with apolo manage state

--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -1,5 +1,7 @@
 import Link from "next/link";
 import NavStyles from "./styles/NavStyles";
+import { Mutation } from "react-apollo";
+import { TOGGLE_CART_MUTATION } from "./Cart";
 import Signout from "./Signout";
 import User from "./User";
 
@@ -22,6 +24,9 @@ const Nav = () => (
               <a>Account</a>
             </Link>
             <Signout />
+            <Mutation mutation={TOGGLE_CART_MUTATION}>
+              {toggleCart => <button onClick={toggleCart}>My Cart</button>}
+            </Mutation>
           </>
         )}
         {!me && (
