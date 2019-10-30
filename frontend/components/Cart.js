1323767e92d73e542716d20041aa9139e922ee49
Zenjutahi <zenjutahi@gmail.com>
2019-10-30 17:27:21 +0300

    [feature] - display cart items and totals

--- frontend/components/Cart.js
+++ frontend/components/Cart.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { Query, Mutation } from "react-apollo";
 import gql from "graphql-tag";
+import User from "./User";
 import CartStyles from "./styles/CartStyles";
 import Supreme from "./styles/Supreme";
 import CloseButton from "./styles/CloseButton";
 import SickButton from "./styles/SickButton";
+import CartItem from "./CartItem";
 
 const LOCAL_STATE_QUERY = gql`
   query {
@@ -19,27 +21,43 @@ const TOGGLE_CART_MUTATION = gql`
 `;
 
 const Cart = () => (
-  <Mutation mutation={TOGGLE_CART_MUTATION}>
-    {toggleCart => (
-      <Query query={LOCAL_STATE_QUERY}>
-        {({ data }) => (
-          <CartStyles open={data.cartOpen}>
-            <header>
-              <CloseButton onClick={toggleCart} title="close">
-                &times;
-              </CloseButton>
-              <Supreme>Your Cart</Supreme>
-              <p>You have __ Items in your cart.</p>
-            </header>
-            <footer>
-              <p>$10.10</p>
-              <SickButton>Checkout</SickButton>
-            </footer>
-          </CartStyles>
-        )}
-      </Query>
-    )}
-  </Mutation>
+  <User>
+    {({ data: { me } }) => {
+      if (!me) return null;
+      console.log(me.cart);
+      return (
+        <Mutation mutation={TOGGLE_CART_MUTATION}>
+          {toggleCart => (
+            <Query query={LOCAL_STATE_QUERY}>
+              {({ data }) => (
+                <CartStyles open={data.cartOpen}>
+                  <header>
+                    <CloseButton onClick={toggleCart} title="close">
+                      &times;
+                    </CloseButton>
+                    <Supreme>{me.name}'s Cart</Supreme>
+                    <p>
+                      You have {me.cart.length} Item
+                      {me.cart.length === 1 ? "" : "s"} in your cart.
+                    </p>
+                  </header>
+                  <ul>
+                    {me.cart.map(cartItem => (
+                      <CartItem key={cartItem.id} cartItem={cartItem} />
+                    ))}
+                  </ul>
+                  <footer>
+                    <p>$10.10</p>
+                    <SickButton>Checkout</SickButton>
+                  </footer>
+                </CartStyles>
+              )}
+            </Query>
+          )}
+        </Mutation>
+      );
+    }}
+  </User>
 );
 
 export default Cart;
