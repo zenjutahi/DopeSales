66314ad618d736448a31a987d1361d668ee1fdea
Zenjutahi <zenjutahi@gmail.com>
2019-10-31 16:27:04 +0300

    [feature] - optimistic remove items from cart

--- frontend/components/RemoveFromCart.js
+++ frontend/components/RemoveFromCart.js
@@ -27,11 +27,31 @@ class RemoveFromCart extends React.Component {
   static propTypes = {
     id: PropTypes.string.isRequired
   };
+  // This gets called as soon as we get a response back from the server after the mutation has been performed.
+  update = (cache, payload) => {
+    console.log("Running remove from cart update function");
+    // first read the cache
+    const data = cache.readQuery({ query: CURRENT_USER_QUERY });
+    console.log(data);
+    // Remove that item from the cart
+    const cartItemId = payload.data.removeFromCart.id;
+    data.me.cart = data.me.cart.filter(cartItem => cartItem.id !== cartItemId);
+    // write it back to the cache
+    cache.writeQuery({ query: CURRENT_USER_QUERY, data });
+  };
   render() {
     return (
       <Mutation
         mutation={REMOVE_FROM_CART_MUTATION}
         variables={{ id: this.props.id }}
+        update={this.update}
+        optimisticResponse={{
+          __typename: "Mutation",
+          removeFromCart: {
+            __typename: "CartItem",
+            id: this.props.id
+          }
+        }}
       >
         {(removeFromCart, { loading, error }) => (
           <BigButton
