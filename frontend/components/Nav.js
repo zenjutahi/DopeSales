67f9924d3ee93ca674ba7ce91c5579e3d8031d9c
Zenjutahi <zenjutahi@gmail.com>
2019-11-01 13:37:01 +0300

    [feature] - animating the cart

--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -4,6 +4,7 @@ import { Mutation } from "react-apollo";
 import { TOGGLE_CART_MUTATION } from "./Cart";
 import Signout from "./Signout";
 import User from "./User";
+import CartCount from "./CartCount";
 
 const Nav = () => (
   <User>
@@ -25,7 +26,17 @@ const Nav = () => (
             </Link>
             <Signout />
             <Mutation mutation={TOGGLE_CART_MUTATION}>
-              {toggleCart => <button onClick={toggleCart}>My Cart</button>}
+              {toggleCart => (
+                <button onClick={toggleCart}>
+                  My Cart
+                  <CartCount
+                    count={me.cart.reduce(
+                      (tally, cartItem) => tally + cartItem.quantity,
+                      0
+                    )}
+                  />
+                </button>
+              )}
             </Mutation>
           </>
         )}
