0f6658212ae35bbe82b2caaffab4576f2be4ed49
Zenjutahi <zenjutahi@gmail.com>
2019-11-11 11:48:36 +0300

    [feature] - add user account

--- frontend/components/UserAccount.js
+++ frontend/components/UserAccount.js
@@ -1,9 +1,48 @@
 import React from "react";
+import Head from "next/head";
+import { format, formatDistance } from "date-fns";
+import Error from "./ErrorMessage";
 import User, { CURRENT_USER_QUERY } from "./User";
+import OrderStyles from "./styles/OrderStyles";
 
 class UserAccount extends React.Component {
   render() {
-    return <User>{({ data: { me } }) => <p>Show me this {me.id}</p>}</User>;
+    return (
+      <User>
+        {({ data: { me }, error, loading }) => {
+          if (error) return <Error error={error} />;
+          if (loading) return <p>Loading...</p>;
+          console.log(me);
+          return (
+            <OrderStyles>
+              <Head>
+                <title>Dope Sales - {me.name}</title>
+              </Head>
+              <p>
+                <span>First Name :</span>
+                <span>{me.name}</span>
+              </p>
+              <p>
+                <span>Account Email :</span>
+                <span>{me.email}</span>
+              </p>
+              <p>
+                <span>My Permissions :</span>
+                <span>
+                  {me.permissions.map(perm => (
+                    <span key={perm}>{perm}</span>
+                  ))}
+                </span>
+              </p>
+              <p>
+                <span>Date Joined :</span>
+                <span>{formatDistance(me.createdAt, new Date())}</span>
+              </p>
+            </OrderStyles>
+          );
+        }}
+      </User>
+    );
   }
 }
 
