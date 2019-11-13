b0ff28fa2aa772e8851d39a7acc854f7f7f8cdcc
Zenjutahi <zenjutahi@gmail.com>
2019-11-13 16:02:49 +0300

    [chore] - testing the <Pagination /> component

--- frontend/components/OrderList.js
+++ frontend/components/OrderList.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import { Query } from "react-apollo";
 import { formatDistance } from "date-fns";
 import Link from "next/link";
