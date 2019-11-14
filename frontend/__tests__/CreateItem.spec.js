7438074d731b14029ec3f4211a171ac4ef7f3dc5
Zenjutahi <zenjutahi@gmail.com>
2019-11-14 14:42:47 +0300

    [chore] - testing the <CreateItem /> query component

---
+++ frontend/__tests__/CreateItem.spec.js
@@ -0,0 +1,121 @@
+import { mount } from "enzyme";
+import wait from "waait";
+import toJSON from "enzyme-to-json";
+import Router from "next/router";
+import CreateItem, { CREATE_ITEM_MUTATION } from "../components/CreateItem";
+import { MockedProvider } from "react-apollo/test-utils";
+import { fakeItem } from "../lib/testUtils";
+
+const dogImage = "https://dog.com/dog.jpg";
+
+// mock the global fetch API
+global.fetch = jest.fn().mockResolvedValue({
+  json: () => ({
+    secure_url: dogImage,
+    eager: [{ secure_url: dogImage }]
+  })
+});
+
+describe("<CreateItem", () => {
+  it("renders and matches snapshot", async () => {
+    const wrapper = mount(
+      <MockedProvider>
+        <CreateItem />
+      </MockedProvider>
+    );
+    const form = wrapper.find('form[data-test="form"]');
+    expect(toJSON(form)).toMatchSnapshot();
+  });
+
+  it("uploads a file when changed", async () => {
+    const wrapper = mount(
+      <MockedProvider>
+        <CreateItem />
+      </MockedProvider>
+    );
+    const input = wrapper.find('input[type="file"]');
+    input.simulate("change", { target: { files: ["fakedog.jpg"] } });
+    await wait();
+    const component = wrapper.find("CreateItem").instance();
+    expect(component.state.image).toEqual(dogImage);
+    expect(component.state.largeImage).toEqual(dogImage);
+    expect(global.fetch).toHaveBeenCalled();
+    global.fetch.mockReset();
+    // expect(toJSON(form)).toMatchSnapshot();
+  });
+
+  it("handles state updating", async () => {
+    const item = fakeItem();
+    const wrapper = mount(
+      <MockedProvider>
+        <CreateItem />
+      </MockedProvider>
+    );
+    wrapper
+      .find("#title")
+      .simulate("change", { target: { value: item.title, name: "title" } });
+    wrapper.find("#price").simulate("change", {
+      target: { value: item.price, name: "price", type: "number" }
+    });
+    wrapper.find("#description").simulate("change", {
+      target: { value: item.description, name: "description" }
+    });
+    expect(wrapper.find("CreateItem").instance().state).toMatchObject({
+      title: item.title,
+      price: item.price,
+      description: item.description
+    });
+  });
+
+  it("creates an item when the form is submitted", async () => {
+    const item = fakeItem();
+    const mocks = [
+      {
+        request: {
+          query: CREATE_ITEM_MUTATION,
+          variables: {
+            title: item.title,
+            description: item.description,
+            image: "",
+            largeImage: "",
+            price: item.price
+          }
+        },
+        result: {
+          data: {
+            createItem: {
+              ...fakeItem,
+              id: "abc1234",
+              __typename: "Item"
+            }
+          }
+        }
+      }
+    ];
+
+    const wrapper = mount(
+      <MockedProvider mocks={mocks}>
+        <CreateItem />
+      </MockedProvider>
+    );
+    // simulate someone filling out the form
+    wrapper
+      .find("#title")
+      .simulate("change", { target: { value: item.title, name: "title" } });
+    wrapper.find("#price").simulate("change", {
+      target: { value: item.price, name: "price", type: "number" }
+    });
+    wrapper.find("#description").simulate("change", {
+      target: { value: item.description, name: "description" }
+    });
+    // mock the router
+    Router.router = { push: jest.fn() };
+    wrapper.find("form").simulate("submit");
+    await wait(50);
+    expect(Router.router.push).toHaveBeenCalled();
+    expect(Router.router.push).toHaveBeenCalledWith({
+      pathname: "/item",
+      query: { id: "abc1234" }
+    });
+  });
+});
