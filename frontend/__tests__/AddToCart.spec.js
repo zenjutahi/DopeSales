06c07743716e4de37d71ebd8e7049a72a60daf77
Zenjutahi <zenjutahi@gmail.com>
2019-11-18 13:45:20 +0300

    [chore] - testing <AddToCart /> component

--- frontend/__tests__/AddToCart.spec.js
+++ frontend/__tests__/AddToCart.spec.js
@@ -14,13 +14,24 @@ const mocks = [
       data: {
         me: {
           ...fakeUser(),
+          cart: []
+        }
+      }
+    }
+  },
+  {
+    request: { query: CURRENT_USER_QUERY },
+    result: {
+      data: {
+        me: {
+          ...fakeUser(),
           cart: [fakeCartItem()]
         }
       }
     }
   },
   {
-    request: { query: ADD_TO_CART_MUTATION, varaibles: { id: "abc123" } },
+    request: { query: ADD_TO_CART_MUTATION, variables: { id: "abc123" } },
     result: {
       data: {
         addToCart: {
@@ -42,5 +53,50 @@ describe("<AddToCart />", () => {
     await wait();
     wrapper.update();
     expect(toJSON(wrapper.find("button"))).toMatchSnapshot();
+  });
+
+  it("adds an item to cart when clicked", async () => {
+    let apolloClient;
+    const wrapper = mount(
+      <MockedProvider mocks={mocks}>
+        <ApolloConsumer>
+          {client => {
+            apolloClient = client;
+            return <AddToCart id="abc123" />;
+          }}
+        </ApolloConsumer>
+      </MockedProvider>
+    );
+    await wait();
+    wrapper.update();
+    const {
+      data: { me }
+    } = await apolloClient.query({ query: CURRENT_USER_QUERY });
+    expect(me.cart).toHaveLength(0);
+    // add an item to the cart
+    wrapper.find("button").simulate("click");
+    await wait(100);
+    // check if the item is in the cart
+    const {
+      data: { me: me2 }
+    } = await apolloClient.query({ query: CURRENT_USER_QUERY });
+    expect(me2.cart).toHaveLength(1);
+    expect(me2.cart[0].id).toBe("omg123");
+    expect(me2.cart[0].quantity).toBe(3);
+  });
+
+  it("changes from add to adding when clicked", async () => {
+    const wrapper = mount(
+      <MockedProvider mocks={mocks}>
+        <AddToCart id="abc123" />
+      </MockedProvider>
+    );
+    await wait();
+    wrapper.update();
+    expect(wrapper.text()).toContain("Add To Cart");
+    //simulate button click
+    const addButton = wrapper.find("button");
+    addButton.props().onClick();
+    expect(wrapper.text()).toContain("Adding To Cart");
   });
 });
